4178fb5b1bad834cb964972f43d3884a7bed6a82
igorpadua <igorpadua@tutanota.com>
2023-11-13 15:59:44 -0300

    fix: controller para restController

--- src/main/groovy/com/igor/cardapio/controller/FoodController.groovy
+++ src/main/groovy/com/igor/cardapio/controller/FoodController.groovy
@@ -1,15 +1,24 @@
 package com.igor.cardapio.controller
 
-import org.springframework.stereotype.Controller
+import com.igor.cardapio.model.Food
+import com.igor.cardapio.service.FoodService
+import groovy.transform.TypeChecked
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 @RequestMapping("/food")
+@TypeChecked
 class FoodController {
 
-    @GetMapping
-    void getAll() {
+    @Autowired
+    FoodService service;
 
+    @GetMapping
+    List<FoodDTO> getAll() {
+        List<Food> foodList = service.getAll()
+        return foodList
     }
 }
