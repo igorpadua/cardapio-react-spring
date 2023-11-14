0b5340a82540baff04f5892e13e13311205ee1b1
igorpadua <igorpadua@tutanota.com>
2023-11-14 15:43:21 -0300

    feat: card food

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css'
+import {Card} from "./components/card/card.tsx";
+import {FoodData} from "./interface/FoodData.ts";
 
 function App() {
-  return (
-      <div className="App">
-      </div>
-  )
+    const data : FoodData[] = []
+    return (
+        <div className="container">
+            <h1>Cardápio</h1>
+            <div className="card-grid">
+                {data.map(foodData => <Card
+                        price={foodData.price}
+                        title={foodData.title}
+                        image={foodData.image}
+                    />
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default App
